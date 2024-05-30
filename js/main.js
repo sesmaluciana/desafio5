dd830ef03495c9eafd926784af22e09846d6a32f
sesmaluciana <sesmaluciana9@gmail.com>
2024-05-30 18:46:41 -0300

    hice el desafio 5

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let productos = ["heladera","sofá", "tvLG"];
+
+for (let index = 0; index < productos.length; index++){
+  const element = productos[index];
+console.log(element);
+}
+
+for (let index = 0; index < productos.length; index++){
+  const result = productos[index].pop;
+  console.log(result);
+}
